ed0daddb27a324168771c8812f05777d910040cd
Einfachirgendwa1 <faris@karge.berlin>
2024-05-16 17:31:07 +0200

    Aktueller Fortschritt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::{
+    any::{Any, TypeId},
+    fmt::{Display, Write as FmtWrite},
     io::{stdin, stdout, Write},
     process::exit,
 };
 
-trait Get {
+trait Get: Display {
     fn get(&self) -> f32;
 }
 
@@ -28,10 +30,27 @@ enum Operation {
     Geteilt,
 }
 
+impl Display for Operation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_char(match *self {
+            Operation::Plus => '+',
+            Operation::Minus => '-',
+            Operation::Mal => '*',
+            Operation::Geteilt => '/',
+        })
+    }
+}
+
 struct Rechnung<A: Get, B: Get> {
     l: A,
     r: B,
     o: Operation,
+}
+
+impl<A: Get, B: Get> Display for Rechnung<A, B> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{} {} {}", self.l, self.o, self.r))
+    }
 }
 
 impl<A: Get, B: Get> Get for Rechnung<A, B> {
@@ -123,7 +142,34 @@ fn solve(rechnung: &str) -> Result<f32, String> {
         Err(message) => return Err(message),
     };
 
-    dbg!(tokens);
+    let generic_structure: Vec<Box<dyn Get>> = match {
+        let mut result: Vec<Box<dyn Get>> = Vec::new();
+
+        let mut iter = tokens.into_iter();
+        while let Some(token) = iter.next() {
+            match token {
+                Token::Literal(x) => result.push(Box::new(x)),
+                Token::Operation(x) => match result.pop() {
+                    None => {
+                        return Err(
+                            "Links von dem Rechenzeichen wurde eine Zahl erwartet".to_string()
+                        )
+                    }
+                    Some(y) => {
+                        if !(y.type_id() == TypeId::of::<f32>()) {
+                            return Err(format!("Links von dem Rechenzeichen {} wurde eine Zahl erwartet, nicht {}.", x, y));
+                        } else {
+                        }
+                    }
+                },
+            }
+        }
+
+        Ok(result)
+    } {
+        Ok(value) => value,
+        Err(message) => return Err(message),
+    };
 
     Err("Noch nicht implementiert".to_string())
 }
